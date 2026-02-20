03bb3e4c0ce21cfc743bbd4467b81dbeee83f576
Moisha9696 <mikhail.yukhovich@gmail.com>
2026-02-20 13:37:55 +0300

    Initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# docker-express-1
---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+const port = 3000;
+const users = [];
+
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => {
+    res.send('HELLO!')
+});
+
+app.get('/users', (req,res)=>{
+    return res.json(users);
+});
+
+app.post('/users', (req,res)=>{
+    const newId = req.body.userId;
+    if (!newId) {
+        return res.status(400).send('Missing userId.');
+    }
+
+    if (users.includes(newId)){
+        return res.status(400).send('userId already exists.');
+    }
+
+    users.push(newId);
+    return res.status(201).send('user regstered.');
+});
+
+
+
+app.listen(port, ()=>{
+    console.log("STARTED");
+});
